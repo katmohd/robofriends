e56beb534beba5e0e25348c7c0f3da88d20e65c7
katsobri <kartmohd@gmail.com>
2022-09-11 16:50:53 +0800

    Updates

---
+++ precache-manifest.818329d7fda3e382d4ad1fac4d4e918c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a206da5ede9a7b4806",
+    "url": "/robofriends/static/css/main.f1eff1c5.chunk.css"
+  },
+  {
+    "revision": "73a206da5ede9a7b4806",
+    "url": "/robofriends/static/js/main.73a206da.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robofriends/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "7d82f85b9904c25c3f8f",
+    "url": "/robofriends/static/css/2.1939da66.chunk.css"
+  },
+  {
+    "revision": "7d82f85b9904c25c3f8f",
+    "url": "/robofriends/static/js/2.7d82f85b.chunk.js"
+  },
+  {
+    "revision": "5a6fc926011dbcb5a003",
+    "url": "/robofriends/static/js/runtime~main.5a6fc926.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b7dd548c08e8ed7808bc189091093c98",
+    "url": "/robofriends/index.html"
+  }
+];
